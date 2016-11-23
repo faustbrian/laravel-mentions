8eaca8708b0409c0b5baaa6928b1f6f620ef4a91
Brian Faust <hello@brianfaust.de>
2016-11-23 12:16:58 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- src/functions.php
+++ src/functions.php
@@ -1,7 +1,15 @@
 <?php
 
+/*
+ * This file is part of Laravel Mentions.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-if (!function_exists('mention')) {
+if (! function_exists('mention')) {
     /**
      * @return \Illuminate\Foundation\Application|mixed
      */
